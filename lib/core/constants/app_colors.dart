1751644a998ff033887a47483ca230b4e825213f
muminovabdulloh <muminovabdullokh@gmail.com>
2024-07-13 20:03:59 +0500

    Muminov Abdulloh, DM Sans font has added to the project, Some AppTextStyles and AppColors have added.

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
+@immutable
+sealed class AppColors {
   static const Color primary = Color(0xFF0A0E21);
   static const Color accent = Color(0xFFEB1555);
   static const Color background = Color(0xFFFFFFFF);
+  static const Color dark = Color(0xff1B1D21);
+  static const Color dark50 = Color.fromRGBO(0, 0, 0, 0.5);
+
 }
